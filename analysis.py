77a7a62c7b5abcb38f9174a15bc2689b1d16be33
legol <legolas2002@mail.ru>
2023-12-01 16:42:52 +0300

    First files was created

---
+++ analysis.py
@@ -0,0 +1,153 @@
+import numpy as np
+from tqdm import tqdm
+
+
+class Analyzer:
+    def __init__(self, min_wrist_dist=110, max_wrist_dist=130, max_wrist_move=10):
+        self.min_wrist_dist = min_wrist_dist
+        self.max_wrist_dist = max_wrist_dist
+        self.max_wrist_move = max_wrist_move
+        self.violations = []
+
+    def _get_center(self, bbox):
+        return np.array([(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2])
+
+    def process_model_data(self, pos_est_data, detection_data):
+        violations = []
+
+        for frame_id in pos_est_data.index:
+            persons_wrists = [pose[9:11] for pose in pos_est_data.loc[frame_id, 'KeyPoints']]
+            time = pos_est_data.loc[frame_id, 'Time']
+
+            if frame_id in detection_data.index:
+                phones = detection_data.loc[frame_id, 'Position'].copy()
+
+                for i in range(len(persons_wrists)):
+
+                    for j in range(len(persons_wrists[i])):
+                        dists = list(map(lambda x: np.linalg.norm(self._get_center(x) - persons_wrists[i][j]), phones))
+                        min_dist = min(dists)
+
+                        closest_id = dists.index(min_dist)
+                        closest_phone = phones[closest_id]
+
+                        if min_dist <= self.max_wrist_dist:
+                            if not ((j == 0 and persons_wrists[i][j][0] > closest_phone[2])
+                                    or (j == 1 and persons_wrists[i][j][0] < closest_phone[0])):
+                                violations.append([frame_id, time])
+        return violations
+
+    def _update_violations(self, violations_processed, passed_time, start_time):
+        if passed_time > 3000:
+            total_sec = start_time / 1000
+            min = int(total_sec // 60)
+            sec = int(total_sec % 60)
+
+            total_p_sec = (start_time + passed_time) / 1000
+            p_min = int(total_p_sec // 60)
+            p_sec = int(total_p_sec % 60)
+
+            violations_processed.append([f'{min}:{sec:02}', f'{p_min}:{p_sec:02}'])
+        return violations_processed
+
+    def process_violations(self, violations):
+        violations_processed = []
+        passed_time = 0
+        start_time = -1
+
+        for i in range(0, len(violations) - 1):
+            time_diff = violations[i + 1][1] - violations[i][1]
+
+            if (passed_time < 3000 and time_diff <= 2000) or (passed_time > 3000 and time_diff <= 10000):
+                if start_time == -1:
+                    start_time = violations[i][1]
+                passed_time += time_diff
+
+                if i == len(violations) - 2:
+                    violations_processed = self._update_violations(violations_processed, passed_time, start_time)
+            else:
+                violations_processed = self._update_violations(violations_processed, passed_time, start_time)
+
+                passed_time = 0
+                start_time = -1
+        return violations_processed
+
+    def full_process_model_data(self, pos_est_data, detection_data):
+        history = []
+        max_allowed_dist = 1080 / 2
+        wrist_phone_dist = 90
+        violations = []
+
+        for frame_id in pos_est_data.index:
+            print(frame_id, len(violations), len(history))
+
+            persons_wrists = [pose[9:11] for pose in pos_est_data.loc[frame_id, 'KeyPoints']]
+            persons_shoulders = [pose[5:7] for pose in pos_est_data.loc[frame_id, 'KeyPoints']]
+            time = pos_est_data.loc[frame_id, 'Time']
+
+            if frame_id in detection_data.index:  # we need to update phones last positions
+                phones = detection_data.loc[frame_id, 'Position'].copy()
+                visibility = [0 for i in range(len(history))]
+
+                if len(history) < len(phones):
+                    for i in range(len(history)):
+                        center = self._get_center(history[i])
+                        dists = list(map(lambda x: np.linalg.norm(self._get_center(x) - center), phones))
+                        min_dist = min(dists)
+
+                        if min_dist < max_allowed_dist:
+                            closest_id = dists.index(min_dist)
+                            history[i] = phones[closest_id]
+                            visibility[i] = 1  # we can see phone on frame
+                            phones.pop(closest_id)
+
+                    if phones:
+                        history.extend(phones)
+                        visibility.extend([1 for i in range(len(phones))])  # we can see all extended phones
+
+                else:
+                    unhandled = []
+                    for j in range(len(phones)):
+                        center = self._get_center(phones[j])
+                        dists = list(map(lambda x: np.linalg.norm(self._get_center(x) - center), history))
+                        min_dist = min(dists)
+
+                        if min_dist < max_allowed_dist:
+                            closest_id = dists.index(min_dist)
+                            history[closest_id] = phones[j]
+                            visibility[closest_id] = 1
+                        else:
+                            unhandled.append(phones[j])
+                    if unhandled:
+                        history.extend(unhandled)
+                        visibility.extend([1 for i in range(len(unhandled))])  # we can see all extended phones
+
+            # if we have info about phones pos
+            if history:
+                # wrist near phone
+                for i in range(len(persons_wrists)):
+                    for wrists in persons_wrists[i]:
+                        dists = list(map(lambda x: np.linalg.norm(self._get_center(x) - wrists), history))
+                        min_dist = min(dists)
+                        closest_id = dists.index(min_dist)
+                        if min_dist <= wrist_phone_dist:
+                            violations.append([frame_id, time, visibility[closest_id]])
+
+                # hidden phone between shoulders
+                for i in range(len(history)):
+                    if visibility[i] == 0:  # for each invisible phone
+                        center = self._get_center(history[i])
+
+                        for j in range(len(persons_shoulders)):  # for each person
+                            shoulders = persons_shoulders[j]
+                            wrists = persons_wrists[j]
+                            if shoulders[0][0] < center[0] < shoulders[1][0] and (
+                                    shoulders[0][0] < wrists[0][0] < shoulders[1][0] or shoulders[0][0] < wrists[1][0] <
+                                    shoulders[1][0]
+                            ):
+                                violations.append([frame_id, time, 0])
+        return violations
+
+    def get_violations(self, pos_est_data, detection_data):
+        violations = self.process_model_data(pos_est_data, detection_data)
+        return self.process_violations(violations)
---
+++ model.py
@@ -0,0 +1,131 @@
+import time
+
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from ultralytics import YOLO
+
+from utils import plot_boxes
+
+
+class AttentionModel:
+    def __init__(self, batch_size=4, grayscale_adapted=False):
+        self.detected_history = []
+        self.pos_est_values = []
+        self.detected_values = []
+        self.sec_per_frame = None
+        self.batch_size = batch_size
+        self.grayscale_adapted = grayscale_adapted
+
+        # models
+        self.detect_model = None,
+        self.detect_model_classes = None
+        self.pos_est_model = None
+
+        # video parameters
+        self.video_type = 'avi'  # saved video type
+
+    def load_models(self, detection_modelname=None, pose_estimator_name=None):
+        if detection_modelname:
+            self.detect_model = YOLO(detection_modelname)
+            self.detect_model_classes = self.detect_model.names
+            self.detect_model.to('cuda')
+        if pose_estimator_name:
+            self.pos_est_model = YOLO(pose_estimator_name)
+            self.pos_est_model.to('cuda')
+
+    def handle_detection(self, results, frames, timestamps, frame_ids, save=False):
+        for i in range(len(results)):
+            detected_classes = []
+            detected_positions = []
+            boxes = results[i].boxes.cpu().numpy()
+
+            for box in boxes:
+                class_name = self.detect_model_classes[int(box.cls)]
+                detected_classes.append(str(class_name))
+                xyxy = box.xyxy[0]
+
+                if save:
+                    confidence = str(round(box.conf[0].item(), 2))
+                    label = f'{class_name}: {confidence}'
+                    frames[i] = plot_boxes(frames[i], xyxy, label)
+                detected_positions.append(xyxy.tolist())
+
+            if len(detected_classes) != 0:
+                self.detected_values.append([frame_ids[i], timestamps[i], detected_positions])
+        return frames
+
+
+    def handle_pos_est(self, results, frames, timestamps, frame_ids, save=False):
+        for i in range(len(results)):
+            keypoints = results[i].keypoints.xy.cpu().numpy().tolist()
+            for pose in keypoints:
+                if save:
+                    for point in pose:
+                        frames[i] = cv2.circle(frames[i], (int(point[0]), int(point[1])), radius=0, color=(0, 255, 0), thickness=10)
+            self.pos_est_values.append([frame_ids[i], timestamps[i], len(results[i]), keypoints])
+        return frames
+
+    def process_batch(self, frames, timestamps, frame_ids, save):
+        if self.pos_est_model is not None:
+            pos_est_results = self.pos_est_model(frames, verbose=False)
+            frames = self.handle_pos_est(pos_est_results, frames, timestamps, frame_ids, save)
+        if self.detect_model is not None:
+            conf = 0.8
+            if self.grayscale_adapted and np.array(frames).std(axis=-1).mean() < 2:
+                conf = 0.4
+            detection_results = self.detect_model(frames, verbose=False, conf=conf)
+            frames = self.handle_detection(detection_results, frames, timestamps, frame_ids, save)
+        return frames
+
+    def process_video(self, data_path, out_path, detection=True, pos_estimation=True, save=True):
+        cap = cv2.VideoCapture(data_path)
+        fps = cap.get(cv2.CAP_PROP_FPS)
+
+        self.sec_per_frame = 1 / fps
+        self.detected_values = []
+        self.pos_est_values = []
+        self.detected_history = []
+        self.use_detection = detection
+        self.use_pose_estimation = pos_estimation
+
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        frame_width = int(cap.get(3))
+        frame_height = int(cap.get(4))
+
+        codec = cv2.VideoWriter_fourcc('M','J','P','G')  # avi format
+        out = cv2.VideoWriter(out_path, codec, fps, (frame_width, frame_height))
+
+        start = time.time()
+        with tqdm(total=frame_count) as pbar:
+            while cap.isOpened():
+                frames = []
+                frame_ids = []
+                timestamps = []
+                for i in range(self.batch_size):
+                    success, frame = cap.read()
+                    if not success:
+                        break
+                    timestamps.append(cap.get(cv2.CAP_PROP_POS_MSEC))
+                    frame_ids.append(int(cap.get(cv2.CAP_PROP_POS_FRAMES)))
+                    frames.append(frame)
+                if len(frames) != 0:
+                    frames = self.process_batch(frames, timestamps, frame_ids, save)
+                    if save:
+                        for frame in frames:
+                            out.write(frame)
+                if not success:
+                    break
+                pbar.update(len(frames))
+        end = time.time() - start
+        print(f'Time: {end}')
+        self.pos_est_data = pd.DataFrame(self.pos_est_values, columns=['Frame_id', 'Time', 'People_count', 'KeyPoints'])
+        self.detected_data = pd.DataFrame(self.detected_values, columns=['Frame_id', 'Time', 'Position'])
+
+    def get_detected_data(self):
+        return self.detected_data
+
+    def clear_data(self):  # clear detected_data
+        self.detected_data = self.detected_data.iloc[0:0]
